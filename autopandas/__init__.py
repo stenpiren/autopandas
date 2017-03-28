568316a27b59139c2cb5f8f5fd7168f481ee723b
tvaroska <boris@tvaroska.sk>
2017-03-28 12:55:56 -0500

    Create autopandas/__init__.py

---
+++ autopandas/__init__.py
@@ -0,0 +1,119 @@
+class autopandas(BaseEstimator, TransformerMixin):
+    def __init__(self, level = 0, ignore=[], drop_at = 0.8, categories = 0.1, **kwargs):
+
+        self.level = level
+
+        self.drop_at    = drop_at
+        self.categories = categories
+        
+        self.ignore  = ignore
+
+        self.map = {}
+        self.pipeline = []
+
+    def __str__(self, **kwargs):
+        if self.map == {}:
+            return super().__str__(**kwargs)
+
+        ret = ""
+        for column in self.map:
+            ret = ret + column + "\t\t" + str(self.map[column][0]) + "\n"
+        return(ret)
+
+    def fit(self, X, y=None):
+        non_empty = X.count()
+        for column in titanic.columns:
+            if column in self.ignore:
+                continue
+            complete    = ("full" if X.shape[0] == non_empty[column] else "partial")
+            unique      = len(titanic[column].unique())
+
+            # TODO: categorical identification
+            if X.dtypes[column] in [np.float64, np.int64]:
+                i_type = str(X.dtypes[column])
+            elif X.dtypes[column] == np.object:
+                # Categorical identification:
+                if unique <= non_empty[column] * self.categories:
+                    i_type = "categorical"
+                else:
+                    i_type = np.nan
+            else:
+                i_type      = np.nan
+
+            self.map[column] = (X.dtypes[column], i_type, complete, unique)
+            
+# Level 0 - full
+        l0 = []
+        for key in self.map:
+            if not(self.map[key][1] is np.nan) and (self.map[key][2] == 'full'):
+                m_str = 'l0_{0}_{1}'.format(self.map[key][1], self.map[key][2])
+                method = getattr(self, m_str)
+                line   = method(key)
+                try:
+                    reshape = line[2]
+                except:
+                    reshape = True
+                if reshape:
+                    line[1].fit(X[key].reshape(-1,1))
+                else:
+                    line[1].fit(X[key])
+                l0.append(line)
+        self.pipeline.append(l0)
+
+# Level 0 - partial
+        l05 = []
+        for key in self.map:
+            if not(self.map[key][1] is np.nan) and (self.map[key][2] == 'partial'):
+                m_str = 'l0_{0}_{1}'.format(self.map[key][1], self.map[key][2])
+                method = getattr(self, m_str)
+                line   = method(key)
+                try:
+                    reshape = line[2]
+                except:
+                    reshape = True
+                if reshape:
+                    line[1].fit(X[key].reshape(-1,1))
+                else:
+                    line[1].fit(X[key])
+                l05.append(line)
+        self.pipeline.append(l05)
+
+        return self
+
+
+    def transform(self, X, y=None):
+        res = None
+        for pip in self.pipeline:
+            for column in pip:
+                try:
+                    reshape = column[2]
+                except:
+                    reshape = True
+                if reshape:
+                    data = column[1].transform(X[column[0]].reshape(-1,1))
+                else:
+                    data = column[1].transform(X[column[0]])
+                if res is None:
+                    res = data
+                else:
+                    res = np.concatenate((res, data), axis=1)
+
+        return res
+
+    def l0_int64_full(self, column):
+        return [column, Pipeline([("imputer", Imputer()), ("scale", RobustScaler())])]
+
+    def l0_float64_full(self, column):
+        return self.l0_int64_full(column)
+
+    def l0_float64_partial(self, column):
+        return self.l0_int64_full(column)
+
+    def l0_int64_partial(self, column):
+        return self.l0_int64_full(column)
+
+    def l0_categorical_full(self, column):
+        return [column, Pipeline([("categorical", CategoryTransformer(column = column, dropna = False))]), False]
+
+    def l0_categorical_partial(self, column):
+        return [column, Pipeline([("categorical", CategoryTransformer(column = column, dropna = True))]), False]
